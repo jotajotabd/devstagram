5d18703ffc5501a83a5031a255837aa96df3db98
JotajotaBD <jberroteranj@gmail.com>
2024-08-28 16:12:25 -0400

    Se trabajó con las BD, las migraciones (username) y los modelos

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -15,12 +18,24 @@ class RegisterController extends Controller
     {
         // dd($request);
         // dd($request-> get('username'));
+
+        // Modificar request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        // Validación
         $this->validate($request,[
             'name' => 'required|min:3|max:10',
             'username' => 'required|unique:users|min:10|max:30',
-            'email' => 'required|unique:email|max:30',
+            'email' => 'required|unique:users|email|max:30',
             'password' => 'required|confirmed|min:6',
             'password_confirmation' => 'required'
+        ]);
+
+        User::create([
+            'name' => $request -> name,
+            'username' => $request -> username,
+            'email' => $request -> email,
+            'password' => Hash::make($request -> password)
         ]);
     }
 }
