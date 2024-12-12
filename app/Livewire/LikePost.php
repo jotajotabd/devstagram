54796ffcc9e73dfee39bf6f08a6afb4dcd4bdec8
JotajotaBD <jberroteranj@gmail.com>
2024-12-12 11:04:17 -0400

    wip: termina lógica del componente livewire de likes, reemplazando al controlador likecontroller

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -8,25 +8,30 @@ class LikePost extends Component
 {
     public $post;
     public $isLiked;
+    public $likes;
 
     public function mount($post)
     {
         $this->isLiked = $post->checkLike(auth()->user()) ? true : false;
+        $this->likes = $post->likes->count();
     }
     public function like()
     {
-        // dd($this->post->likes);
+        // dd($this->likes);
 
         if ($this->post->checkLike(auth()->user()))
         {
             auth()->user()->likes()->where('post_id', $this->post->id)->delete();
             $this->isLiked = false;
+            $this->likes--;
         }else
         {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
             $this->isLiked = true;
+            $this->likes++;
+
         }
     }
 
